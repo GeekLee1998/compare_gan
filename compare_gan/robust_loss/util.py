fcbc76355bd80d966f66121da162fbb488f3b418
GeekLee1998 <948899165@qq.com>
2020-03-16 23:43:12 +0800

    change expm

--- compare_gan/robust_loss/util.py
+++ compare_gan/robust_loss/util.py
@@ -8,6 +8,6 @@ def log1p_safe(x):
   return tf.math.log1p(tf.minimum(x, tf.cast(3e37, x.dtype)))
 
 
-def exp_safe(x):
-  """The same as tf.math.exp(x), but clamps the input to prevent NaNs."""
-  return tf.math.exp(tf.minimum(x, tf.cast(87.5, x.dtype)))
+def expm1_safe(x):
+  """The same as tf.math.expm1(x), but clamps the input to prevent NaNs."""
+  return tf.math.expm1(tf.minimum(x, tf.cast(87.5, x.dtype)))
