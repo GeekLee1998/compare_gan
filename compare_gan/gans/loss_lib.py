1e9366f2f03610c70520b70ccd49d441eed6acd0
GeekLee1998 <948899165@qq.com>
2020-03-23 11:14:30 +0800

    alpha 2

--- compare_gan/gans/loss_lib.py
+++ compare_gan/gans/loss_lib.py
@@ -155,7 +155,7 @@ def hinge(d_real_logits, d_fake_logits, d_real=None, d_fake=None):
 def robust(d_real_logits, d_fake_logits, d_real=None, d_fake=None):
   with tf.name_scope("robust_loss"):
     check_dimensions(d_real, d_fake, d_real_logits, d_fake_logits)
-    alpha = tf.zeros([64,1],tf.float32)
+    alpha = tf.constant(2.0,shape=[64,1])
     scale = tf.random_uniform([64,1],0.1,1)
     d_loss_real = general.lossfun(d_real_logits,alpha,scale)
     d_loss_fake = general.lossfun(d_fake_logits,alpha,scale)
